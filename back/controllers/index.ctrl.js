392d6a920187b1bf5b2af34b7588288a723e4a8f
Thanasis Polychronakis <thanpolas@gmail.com>
2014-01-13 20:46:55 +0200

    Remove all fronttoken refs

--- back/controllers/index.ctrl.js
+++ back/controllers/index.ctrl.js
@@ -5,14 +5,11 @@
 var util = require('util');
 
 // var __ = require('lodash');
-var config = require('config');
+// var config = require('config');
 // var log = require('logg').getLogger('cc.ctrl.Homepage');
 
 var Controller = require('./controller');
 var helpers = require('../util/helpers');
-var Set = require('collections/set');
-
-var rpc;
 
 /**
  * The home page.
@@ -27,24 +24,6 @@ var Home = module.exports = function(){
 util.inherits(Home, Controller);
 helpers.addSingletonGetter(Home);
 
-Home.frontTokenSet = new Set();
-
-/**
- * Get a unique front token
- *
- * @return {string} a unique token.
- * @static
- */
-Home.getFrontToken = function() {
-  var frontToken = helpers.generateRandomString(6);
-
-  // TODO check if returns false and regenerate
-  Home.frontTokenSet.add(frontToken);
-
-  return frontToken;
-};
-
-
 /**
  * The index page.
  *
@@ -52,11 +31,7 @@ Home.getFrontToken = function() {
  * @param {Object} res The response Object.
  */
 Home.prototype._useIndex = function(req, res) {
-  var frontToken = Home.getFrontToken();
-  rpc.send('frontToken', frontToken);
   res.render('index', {
-    frontToken: frontToken,
     ip: req.connection.remoteAddress,
-    echoServer: config.echoServer,
   });
 };
